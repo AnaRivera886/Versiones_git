581e17c965028be6f2ae540dd74f530c9c90abce
Ana Rivera <asrivera886@gmail.com>
2025-10-30 06:31:36 -0500

    Clase 2 Tarea git - github

--- CLASE_2/ARCHIVOS_JS/js/3.js
+++ CLASE_2/ARCHIVOS_JS/js/3.js
@@ -1,16 +1,16 @@
 let pares = 0;
 let impares = 0;
-let i = 0;
+let i = 1;
+
 while (i <= 30) {
-    if (i % 2 === 0) {
-        pares=+1;
-        i=+1;
-        console.log("Numeros pares" + i);
-        
-    }
+    if (i % 2 == 0) {
+        console.log(i +  "Es par")
+        pares++;       
+        i++; 
+    }   
     else {
-        impares=+1;
-        i=+1;
-        console.log("Numeros impares" + i);
+        console.log(i +  "Es impar")
+        impares++;
+        i++;
     }
 }
